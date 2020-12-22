61a6b041b743d737f340d519b1c5715030fc04a7
Ravi <ravinder9.reddy@gmail.com>
2020-12-22 20:25:52 +0530

    Built basic login page

--- frontend/asset-management/src/app/app-routing.module.ts
+++ frontend/asset-management/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   {path: '', component:LoginComponent},
-  {path: 'login', component:LoginComponent}
+  {path: 'login', component:LoginComponent},
+  {path: 'dashboard', component:DashboardComponent},
+  {path: '**', component:ErrorComponent}
 ];
 
 @NgModule({
--- frontend/asset-management/src/app/app.module.ts
+++ frontend/asset-management/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    LoginComponent
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
